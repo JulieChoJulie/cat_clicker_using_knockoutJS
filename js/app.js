43c519dd589a013df9f905186107f41d10c764dd
JulieChoJulie <h32cho@gmail.com>
2018-06-25 21:21:04 -0400

    separating the model out

--- js/app.js
+++ js/app.js
@@ -1,29 +1,37 @@
 var ViewModel = function(){
+    this.currentCat = ko.observable(new Cat());
+    this.incrementCounter = function(){
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
+};
+
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('http://flickr.');
     this.catLevel = ko.observable('newborn');
-    this.incrementCounter = function(){
-        this.clickCount(this.clickCount() + 1);
-        if(this.clickCount() >= 100){
-            this.catLevel('infant');
-        } else if (this.clickCount() >= 10 && this.clickCount < 100){
-            this.catLevel('infant');
-        }
-    };
-    // this.nickName = ko.observableArray([
-    //     {name: 'tom'},
-    //     {name: 'orange'},
-    //     {name: 'nabi'},
-    //     {name: 'goldy'}
-    // ]);
     this.nickName = ko.observableArray([
         'tom',
         'orange',
         'nabi',
         'goldya',
-    ])
-}
+    ]);
+    this.catLevel = ko.computed(function(){
+        if(this.clickCount() < 10){
+            return 'newborn';
+        } else if (this.clickCount() < 50) {
+            return 'infant';
+        } else if (this.clickCount() < 100 ) {
+            return 'child';
+        } else if (this.clickCount() < 200) {
+            return 'teen';
+        } else if (this.clickCount() < 500) {
+            return 'adult';
+        } else {
+            return 'ninja';
+        }
+    }, this);
+};
 
 ko.applyBindings(new ViewModel());
