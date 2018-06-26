29d0d81107f03bc5e92930704a3414ad57c625b6
JulieChoJulie <h32cho@gmail.com>
2018-06-25 23:14:31 -0400

    final

--- js/app.js
+++ js/app.js
@@ -1,22 +1,75 @@
 var ViewModel = function(){
-    this.currentCat = ko.observable(new Cat());
-    this.incrementCounter = function(){
-        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
-    };
-};
+    var that = this;
+    this.catList = ko.observableArray([]);
 
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('http://flickr.');
-    this.catLevel = ko.observable('newborn');
-    this.nickName = ko.observableArray([
-        'tom',
-        'orange',
-        'nabi',
-        'goldya',
-    ]);
+    initialCat.forEach(cat => {
+        //in catList => [Cat {clickCount: f, name: f, imgSrc: f...}, Cat {clickCount: f...}
+        that.catList.push(new Cat(cat));
+    });
+    //that.catList()[0] = Cat {clickCount: f, ..}
+    //this.currentCat = function(){Cat {clickCount: f, ...}
+    this.currentCat = ko.observable(that.catList()[0]);
+    this.incrementCounter = function(){
+        that.currentCat().clickCount(that.currentCat().clickCount() + 1);
+    };
+    this.changePhoto = function(cat){
+        //input cat = Cat {clickCount: f, name: f, imgSrc: f, ..}
+        that.currentCat(cat);
+    }
+};
+var initialCat = [
+    {
+        'clickCount': 0,
+        'name': 'Tabby',
+        'imgSrc': 'img/1.jpg',
+        'nickName': [
+            'tom',
+            'orange',
+            'nabi',
+            'goldya',
+        ]},
+    {
+        'clickCount': 0,
+        'name': 'Tiger',
+        'imgSrc': 'img/2.jpg',
+        'nickName': [
+            'tiny',
+            'tny'
+        ]},
+    {
+        'clickCount': 0,
+        'name': 'Lobby',
+        'imgSrc': 'img/3.jpg',
+        'nickName': [
+            'lovely',
+            'lob'
+        ]},
+    {
+        'clickCount': 0,
+        'name': 'Kitty',
+        'imgSrc': 'img/4.jpg',
+        'nickName': [
+            'Katty',
+            'kammy'
+        ]},
+    {
+        'clickCount': 0,
+        'name': 'Smokey',
+        'imgSrc': 'img/5.jpg',
+        'nickName': [
+            'Smukey',
+            'Smok',
+            'Snukey'
+        ]}
+];
+
+//this Cat is making {clickCount: function, name: function, imgSrc:function, ...}
+//an instance of a user-defined object type
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.nickName = ko.observableArray(data.nickName);
     this.catLevel = ko.computed(function(){
         if(this.clickCount() < 10){
             return 'newborn';
